b4ca6622fcb623a95ab818b2f1ab42b2b63f40e7
Robert Siński <robert.sinski@outlook.com>
2024-10-11 20:01:22 +0200

    chore: first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div
+      className="flex flex-col items-center justify-center h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/bg.png')" }}
+    >
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+  );
+}
