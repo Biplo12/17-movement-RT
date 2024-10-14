7d3d35967a641ef418130f00396e55bb9e4d838d
Robert Siński <robert.sinski@outlook.com>
2024-10-14 17:25:02 +0200

    refactor: Add expand functionality to CardFunctionHeader component

--- src/components/card-function-header.tsx
+++ src/components/card-function-header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface CardFunctionHeaderProps {
   title: string;
@@ -9,12 +9,27 @@ const CardFunctionHeader: React.FC<CardFunctionHeaderProps> = ({
   title,
   icon,
 }): JSX.Element => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="flex items-center justify-start gap-4 py-5 px-7 bg-[#0f0f14] w-full">
-      {icon && (
-        <img src={`/icons/${icon}.svg`} alt={icon} className="w-5 h-5" />
-      )}
-      <h2 className="text-sm font-bold">{title}</h2>
+    <div className="flex items-center justify-between gap-4 py-5 px-7 bg-[#0f0f14] w-full">
+      <div className="flex items-center justify-start gap-2">
+        {icon && (
+          <img src={`/icons/${icon}.svg`} alt={icon} className="w-5 h-5" />
+        )}
+        <h2 className="text-sm font-bold">{title}</h2>
+      </div>
+      <button onClick={handleExpand}>
+        <img
+          src={`/icons/${isExpanded ? "arrow-up-icon" : "arrow-down-icon"}.svg`}
+          alt="arrow-up"
+          className="w-3 h-3"
+        />
+      </button>
     </div>
   );
 };
